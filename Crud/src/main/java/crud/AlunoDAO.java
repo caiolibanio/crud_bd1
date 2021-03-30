52fa3247f3fa4e1f98fc4721ed1babd8b0284cc4
Caio <caiolibanio@gmail.com>
2021-03-29 21:15:45 -0300

    Updated methods

--- Crud/src/main/java/crud/AlunoDAO.java
+++ Crud/src/main/java/crud/AlunoDAO.java
@@ -7,8 +7,8 @@ import javax.persistence.Persistence;
 /**
  * 
  * @author caio 
- * Os métodos de acesso aos dados aqui no DAO ficarão em branco, para ir discutindo e 
- * escrevendo eles com os alunos durante a aula
+ * Os métodos de acesso aos dados aqui no DAO estão em branco, para implementacao 
+ * durante a aula.
  *
  */
 public class AlunoDAO {
@@ -37,59 +37,27 @@ public class AlunoDAO {
 
         return entityManager;
        }
-
+       
        public Aluno getById(int id) {
-         return entityManager.find(Aluno.class, id);
+    	 //TODO
+         return new Aluno();
        }
 
-       @SuppressWarnings("unchecked")
-       public List<Aluno> findAll() {
-         return entityManager.createQuery("FROM " +
-         Aluno.class.getName()).getResultList();
-       }
 
        public void persist(Aluno aluno) {
-         try {
-            entityManager.getTransaction().begin();
-            entityManager.persist(aluno);
-            entityManager.getTransaction().commit();
-         } catch (Exception ex) {
-            ex.printStackTrace();
-            entityManager.getTransaction().rollback();
-         }
+    	   //TODO
        }
 
        public Aluno update(Aluno aluno) {
-    	 Aluno upd_Cliente = null;
-         try {
-            entityManager.getTransaction().begin();
-            upd_Cliente = entityManager.merge(aluno);
-            entityManager.getTransaction().commit();
-         } catch (Exception ex) {
-            ex.printStackTrace();
-            entityManager.getTransaction().rollback();
-         }
-         return upd_Cliente;
+    	 //TODO
+         return new Aluno();
        }
 
        public void remove(Aluno aluno) {
-         try {
-            entityManager.getTransaction().begin();
-            aluno = entityManager.find(Aluno.class, aluno.getMatricula());
-            entityManager.remove(aluno);
-            entityManager.getTransaction().commit();
-         } catch (Exception ex) {
-            ex.printStackTrace();
-            entityManager.getTransaction().rollback();
-         }
+    	 //TODO
        }
 
        public void removeById(int id) {
-         try {
-            Aluno aluno = getById(id);
-            remove(aluno);
-         } catch (Exception ex) {
-            ex.printStackTrace();
-         }
+    	 //TODO
        }
 }
